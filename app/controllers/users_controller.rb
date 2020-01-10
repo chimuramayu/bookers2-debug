be569e534418ce4f027bf3b33efbd9eb07c72c63
chimuramayu <chimuramayu@gmail.com>
2020-01-10 20:55:47 +0900

    [fix] users controllerを編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-  	@user = current_user
+  	@user = User.find(params[:id])
   end
 
   def update
-  	@user = current_user
+  	@user = User.find(params[:id])
   	if @user.update(user_params)
   		redirect_to user_path(@user), notice: "successfully updated user!"
   	else
