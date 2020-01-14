8df4dc4b874e6aded135a4ea6d6c29a2bcf21d55
chimuramayu <chimuramayu@gmail.com>
2020-01-14 17:42:27 +0900

    [add]フォロー、フォロワー機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show,:index,:edit,:update]
-  resources :books do
+  resources :users, only: [:show,:index,:edit,:update] do
+  	member do
+  		get :following, :followers
+      resource :relationships, only: [:create, :destroy]
+  	end
+  end
+	resources :books do
   	resource :favorites, only: [:create, :destroy]
   	resources :comments, only: [:create, :destroy]
   end
