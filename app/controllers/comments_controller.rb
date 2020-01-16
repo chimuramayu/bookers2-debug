db0cf27b9143199683047b93a67a46c4712ca369
chimuramayu <chimuramayu@gmail.com>
2020-01-16 21:14:45 +0900

    [fix]コメント機能の非同期通信の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,6 @@ class CommentsController < ApplicationController
 
 	def create
 		@book = Book.find(params[:book_id])
-		@comment = Comment.new
 		# comment = Comment.new(comment_params)
 		# comment.user_id = current_user.id
 		# comment.book_id = book.id
